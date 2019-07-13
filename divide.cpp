d4013e8c31e31ba5b0ef264ed1d89d409995e7de
Marlowe Chung <chongliwei@outlook.com>
2019-07-13 14:19:13 +0800

    Second Week

---
+++ divide.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+	int divide(int dividend, int divisor) {
+		if (divisor == 0 || (dividend == INT_MIN && divisor == -1)) {
+			return INT_MAX;
+		}
+		int sign = (dividend ^ divisor) >= 0 ? 1 : -1;
+		long long newdividend = labs(dividend);
+		long long newdivisor = labs(divisor);
+		int ans = 0;
+		while (newdividend >= newdivisor) {
+			long long tmp = newdivisor;
+			int m = 1;
+			while (newdividend >= (tmp << 1)) {
+				m = m << 1;
+				tmp = tmp << 1;
+			}
+			newdividend -= tmp;
+			ans += m;
+		}
+		return ans * sign;
+	}
+};
