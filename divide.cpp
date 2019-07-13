148d55c09788e2900f2fd794f0289f825ad54070
Marlowe Chung <chongliwei@outlook.com>
2019-07-13 15:44:35 +0800

    Second Week

--- divide.cpp
+++ divide.cpp
@@ -4,6 +4,9 @@ public:
 		if (divisor == 0 || (dividend == INT_MIN && divisor == -1)) {
 			return INT_MAX;
 		}
+        if (dividend == INT_MIN && divisor == 1) {
+            return INT_MIN;
+        }
 		int sign = (dividend ^ divisor) >= 0 ? 1 : -1;
 		long long newdividend = labs(dividend);
 		long long newdivisor = labs(divisor);
